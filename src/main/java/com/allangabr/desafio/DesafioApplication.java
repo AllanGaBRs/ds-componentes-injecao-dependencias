d4bb02e9c925fc9d7e49471535835be5b29d8cf9
allangabr <allanmediogames@gmail.com>
2025-05-24 17:05:47 -0300

    feat: Component and Dependency Injection Challenge Completed

--- src/main/java/com/allangabr/desafio/DesafioApplication.java
+++ src/main/java/com/allangabr/desafio/DesafioApplication.java
@@ -1,13 +1,44 @@
 package com.allangabr.desafio;
 
+import com.allangabr.desafio.entities.Order;
+import com.allangabr.desafio.services.OrderService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
-public class DesafioApplication {
+public class DesafioApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DesafioApplication.class, args);
-	}
+    private OrderService orderService;
 
+    public DesafioApplication(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DesafioApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Order order1 = new Order(1034,  150.00, 20.00);
+        Order order2 = new Order(2282, 800.00, 10.00);
+        Order order3 = new Order(1309, 95.90, 0.0);
+
+        System.out.println("=== Order 1 ===");
+        System.out.println("Pedido código: " + order1.getCode());
+        System.out.println("Valor total: R$" + String.format("%.2f", orderService.total(order1)));
+
+        System.out.println("\n=== Order 2 ===");
+        System.out.println("Pedido código: " + order2.getCode());
+        System.out.println("Valor total: R$" + String.format("%.2f", orderService.total(order2)));
+
+        System.out.println("\n=== Order 3 ===");
+        System.out.println("Pedido código: " + order3.getCode());
+        System.out.println("Valor total: R$" + String.format("%.2f", orderService.total(order3)));
+    }
 }
---
+++ src/main/java/com/allangabr/desafio/services/ShippingService.java
@@ -0,0 +1,18 @@
+package com.allangabr.desafio.services;
+
+import com.allangabr.desafio.entities.Order;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShippingService {
+
+    public Double shipment(Order order) {
+        if (order.getBasic() < 100.0) {
+            return 20.0;
+        } else if (order.getBasic() < 200.0) {
+            return 12.0;
+        }
+        return 0.0;
+    }
+
+}
